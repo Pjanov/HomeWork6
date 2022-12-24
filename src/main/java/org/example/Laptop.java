b14ec94f92c9ffb30929cfe262e08cf9b0020d1f
Андрей <pjanov84@mail.ru>
2022-12-24 16:06:21 +0300

    Updated the laptop class

--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -1,4 +1,38 @@
 package org.example;
 
 public class Laptop {
+    public float price;
+    private String productManufacturer;
+    private String operatingSystem;
+    private String screenDiagonal;
+    private String displayResolution;
+    private String processor;
+    private String ram;
+    private String ssd;
+
+
+    public Laptop(float price, String productManufacturer, String operatingSystem, String screenDiagonal, String displayResolution, String processor, String ram, String ssd) {
+        this.price = price;
+        this.productManufacturer = productManufacturer;
+        this.operatingSystem = operatingSystem;
+        this.screenDiagonal = screenDiagonal;
+        this.displayResolution = displayResolution;
+        this.processor = processor;
+        this.ram = ram;
+        this.ssd = ssd;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "price=" + price +
+                ", productManufacturer='" + productManufacturer + '\'' +
+                ", operatingSystem='" + operatingSystem + '\'' +
+                ", screenDiagonal='" + screenDiagonal + '\'' +
+                ", displayResolution='" + displayResolution + '\'' +
+                ", processor='" + processor + '\'' +
+                ", ram='" + ram + '\'' +
+                ", ssd='" + ssd + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Laptop laptop = new Laptop(42.435f, "Acer",
+                "Windows 10", "15.6 дюймов",
+                "Full HD", "Core i3", "4 гб", "128 гб");
+
+        System.out.println(laptop);
     }
 }
